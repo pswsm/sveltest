c70c2ec25a92164fcfee734ee089f8f88dd882db
pswsm <pauseawesome@protonmail.com>
2023-01-17 23:31:55 +0100

    [WIP] Navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { each } from "svelte/types/runtime/internal/ssr";
+
+let navElems = [
+{
+text: "aaaaa",
+link: "/aaaaa"
+}
+]
+</script>
+<nav>
+{#each navElems as navElem}
+	<a href={navElem.link}>{navElem.text}</a>
+{/each}
+</nav>
